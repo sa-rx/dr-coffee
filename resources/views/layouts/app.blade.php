5eb7b37803e950ef940c5d4b6cbf5ffc246a5075
sa-rx <s-ra1@outlook.com>
2021-12-25 13:51:03 +0300

    yes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,6 @@
 
 
     <!-- Favicons -->
-    <link href="{{ asset('assets/img/favicon.png') }}" rel="icon">
     <link href="{{ asset('assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
 
     
